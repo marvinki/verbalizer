61e37c4a1d370b4461172246a81741abe724f927
MS <dr-schiller@freenet.de>
2016-06-17 15:07:03 +0200

    consolidation

--- src/org/semanticweb/cogExp/GentzenTree/GentzenTree.java
+++ src/org/semanticweb/cogExp/GentzenTree/GentzenTree.java
@@ -8,6 +8,7 @@ import java.util.Set;
 
 import org.semanticweb.cogExp.inferencerules.AdditionalDLRules;
 import org.semanticweb.cogExp.core.SequentInferenceRule;
+import org.semanticweb.cogExp.OWLAPIVerbaliser.VerbalisationManager;
 import org.semanticweb.cogExp.OWLFormulas.OWLFormula;
 import org.semanticweb.cogExp.OWLFormulas.OWLSymb;
 import org.semanticweb.cogExp.inferencerules.INLG2012NguyenEtAlRules;
@@ -76,7 +77,7 @@ public class GentzenTree {
 		treesteps.put(last_key,step);
 		// System.out.println("enter conclusion with: " + conclusion + " " + last_key);
 		conclusions.put(conclusionid, last_key);
-		last_key++;
+		last_key = last_key + 1;
 		return;
 	}
 	
@@ -89,7 +90,7 @@ public class GentzenTree {
 	}
 	
 	public List<Integer> computePresentationOrder(int i){
-		// System.out.println(formulas);
+		// System.out.println("compute presentation order " + i);
 		List<Integer> results = new ArrayList<Integer>();
 		// find premises
 		// System.out.println("find key " + i);
@@ -111,7 +112,7 @@ public class GentzenTree {
 		}
 		// Reordering for ONLYSOME
 				if (infrule.equals(AdditionalDLRules.ONLYSOME)){
-					System.out.println("Gentzen Tree DEBUG ONLYSOME " + premises.size());
+					// System.out.println("Gentzen Tree DEBUG ONLYSOME " + premises.size());
 					int id1 = premises.get(0);
 					OWLFormula formula1 = formulas.get(premises.get(0));
 					premises.remove(0);
@@ -141,16 +142,19 @@ public class GentzenTree {
 			// if (secondformula.getArgs().get(1).getHead().equals(OWLSymb.EXISTS))
 			//       System.out.println("secondformula content : " + secondformula.getArgs().get(1).getArgs().get(1));
 			// System.out.println("cond 1 " + secondformula.getArgs().get(1).getArgs());
-			// System.out.println("cond 2 " + secondformula.getArgs().get(1).getArgs().get(1));
+		    // System.out.println("cond 2 " + secondformula.getArgs().get(1).getArgs().get(1));
 			// System.out.println("cond 3 " + firstformula.getArgs().get(0));
 			if (secondformula.getArgs().get(1).getArgs()!=null &&
 				secondformula.getArgs().get(1).getArgs().size()>0 &&
 				secondformula.getArgs().get(1).getHead().equals(OWLSymb.EXISTS) &&
 				secondformula.getArgs().get(1).getArgs().get(1).equals(firstformula.getArgs().get(0))){
+				//  System.out.println("I JUST DECIDED NOT TO REORDER PREMISES FOR RULE15");
 				Collections.reverse(premises);
 				// System.out.println("I JUST REORDERED PREMISES FOR RULE15");
 				// System.out.println(premises);
 			} else{
+				// Collections.reverse(premises);
+				// System.out.println("I JUST REORDERED PREMISES FOR RULE15");
 				// System.out.println("I JUST DECIDED NOT TO REORDER PREMISES FOR RULE15");
 				// System.out.println(premises);
 			}
@@ -176,14 +180,21 @@ public class GentzenTree {
 					}
 				}
 		// Now do the actual recursive call
+	    // System.out.println("At step " + i + " rule " + infrule.getName());
 		for (int premi: premises){
 			// System.out.println("examining premise " + premi);
-			// System.out.println("axioms: " + axioms);
+		    // System.out.println("axioms: " + axioms);
 			if (!axioms.contains(premi)){
 				// if premise is not an axiom, recurse
 				// System.out.println("find step for conclusion" + formulas.get(premi));
-				int stepi = conclusions.get(premi);	
-				results.addAll(computePresentationOrder(stepi));
+				int stepi = conclusions.get(premi);
+				// System.out.println("step no " + stepi + "introduced this conclusion");
+				List<Integer> recursiveorder = computePresentationOrder(stepi);
+				for (int record : recursiveorder){
+					if (!results.contains(record))
+					results.add(record);
+				}
+				// System.out.println("resultorder " + results);
 			}
 		}
 		results.add(i);
@@ -230,6 +241,57 @@ public class GentzenTree {
 			allsteps.add(treesteps.get(i).getInfrule());
 		}
 		return allsteps;
+	}
+	
+	public String toDOT(){
+		String result = "";
+		List<Integer> order = computePresentationOrder();
+		// System.out.println("computed presentation order " + order);
+		for (Integer index :order ){
+			GentzenStep step = getStepByID(index);
+			int conclusion = step.getConclusion();
+			String stepname = "index_" + index + step.getInfrule().getShortName();
+			if (stepname.contains("-"))
+				stepname = stepname.replaceAll("-", "minus");
+			List<Integer> premises = step.getPremises();
+			String rulelabel = step.getInfrule().getShortName();
+			for (Integer prem : premises){
+				String nodelabel = VerbalisationManager.prettyPrint(formulas.get(prem));
+				if (nodelabel.length()>30){
+					int i = 0;
+					int fin = 0;
+					while(i<nodelabel.length()/2){
+						int tmp = nodelabel.indexOf("⊓",i+1);
+						if (tmp>0)
+						{
+							i = tmp;
+							fin = i;
+						}
+						tmp = nodelabel.indexOf("⊑",i+1);
+						if (tmp>0){
+							i = tmp;
+							fin = i;
+						}
+						i++;
+					}
+					nodelabel = nodelabel.substring(0, fin) + "\n" +  nodelabel.substring(fin, nodelabel.length());
+				}
+				
+				result +=  "n" + prem.toString() + " -> " + stepname + "R" + conclusion + ";\n"; 
+				//
+				result += "n" + prem.toString() + "[shape=rectangle,label=\"" 
+				+ nodelabel  + "\"];\n";
+			}
+			result +=  stepname + "R" + conclusion + " -> " + "n" + conclusion + ";\n";
+			result += "n" 
+			+ conclusion + "[shape=rectangle,label=\"" 
+					+ VerbalisationManager.prettyPrint(formulas.get(conclusion))  + "\"];\n";
+			result += stepname + "R" 
+					+ conclusion + "[color=lightblue,style=filled,label=\"" 
+					+ rulelabel +  "\"];\n";
+		}
+		// return "";
+		return "digraph " + VerbalisationManager.prettyPrint(formulas.get(getStepByID(order.get(order.size()-1)).getConclusion())) + "\n { ratio=compress \n" + result + "}";
 	}
 	
 }
