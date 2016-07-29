933d17b254f4a9b7c405faec0dcc1e8252411cf4
f3l1xp <felix.paffrath@uni-ulm.de>
2016-07-29 18:23:48 +0200

    added a TextSequenceList Class + doc

---
+++ src/org/semanticweb/cogExp/OWLAPIVerbaliser/TextSequenceList.java
@@ -0,0 +1,87 @@
+package org.semanticweb.cogExp.OWLAPIVerbaliser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * contains zero or more TextElementSequences. All you can do is create a list, add and get/set 
+ * to elements in this List. 
+ * @author fpaffrath
+ *
+ */
+public class TextSequenceList {
+	
+	private List<TextElementSequence> list = new ArrayList<TextElementSequence>();
+
+	
+	/**
+	 * @param list List of TextElements
+	 */
+	public TextSequenceList(List<TextElementSequence> list) {
+		this.list = list;
+	}
+
+	
+	public TextSequenceList(){}
+	
+	
+	/**
+	 * @param index the index of the desired element
+	 * @return the element at index
+	 */
+	public TextElementSequence getElement(int index){
+		
+		TextElementSequence element = new TextElementSequence();
+		
+		try{
+			element = list.get(index);
+		}catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		return element;
+	}
+	
+	
+	/**
+	 * @return number of elements in the list
+	 */
+	public int getSize(){
+		return list.size();
+	}
+	
+	
+	/**
+	 * @param index where the element needs to be set
+	 * @param element  to set
+	 */
+	public void setElement( int index, TextElementSequence element){
+		
+		try {
+			list.set(index, element);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+	
+
+	/**
+	 * @param element to add at the end
+	 */
+	public void add(TextElementSequence element){
+		list.add(element);
+	}
+	
+	
+	/**
+	 * @return all elements of the list as Strings, separated by line breaks
+	 */
+	public String toString(){
+		String listSequence = "";
+		for(int i=0; i<list.size(); i++){
+				 listSequence = listSequence + "\n" + list.get(i);
+		}
+		return listSequence;
+	}
+	
+}
---
+++ src/tests/TextSequenceListTEST.java
@@ -0,0 +1,83 @@
+package tests;
+
+import org.semanticweb.cogExp.OWLAPIVerbaliser.TextElement;
+import org.semanticweb.cogExp.OWLAPIVerbaliser.TextElementSequence;
+import org.semanticweb.cogExp.OWLAPIVerbaliser.TextSequenceList;
+
+public class TextSequenceListTEST {
+
+	/**
+	 * the only purpose of this class is to test the verbaliser.TextSequenceList
+	 * @param args no args needed
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/**
+		 * create a list using the constructor, add, set and get elements of this list
+		 */
+		
+		/*
+		 * pretests
+		 */
+		
+		TextElement text1 = new TextElement("foo1");
+		TextElement text2 = new TextElement("foo2");
+		TextElement isText = new TextElement("is");
+		
+		System.out.println("\n...testing TextElement toString():\n");
+		System.out.println(text2.toString());
+		
+		System.out.println("\n...testing TextElement toHTML():\n");
+		System.out.println(text2.toHTML());
+		
+		
+		TextElementSequence testSequence = new TextElementSequence();
+		testSequence.add(text1);
+		testSequence.add(isText);
+		testSequence.add(text2);
+		
+		TextElementSequence testSequence2 = new TextElementSequence();
+		testSequence2.add(text2);
+		testSequence2.add(isText);
+		testSequence2.add(text1);
+		
+		System.out.println("\n...testing TextElementSequence toString():\n");
+		System.out.println(testSequence.toString());
+		
+		
+		/*
+		 * TextSequenceList tests
+		 */
+		
+		TextSequenceList testList = new TextSequenceList();
+		try{
+		TextElementSequence testElement = testList.getElement(1);
+		System.out.println("\n...testing empty TextSequenceList element.toString():\n");
+		System.out.println(testElement.toString());
+		}catch (Exception e) {
+			System.out.println("\n # Error when referencing an empty TextSequence List \n");
+		}
+		
+		
+		testList.add(testSequence);
+		testList.add(testSequence2);
+		testList.add(testSequence2);
+		testList.add(testSequence2);
+		
+		System.out.println("\n...size of my test list:\n");
+		System.out.println(testList.getSize());
+		
+		
+		
+		TextElementSequence testElement2 = testList.getElement(1);
+		System.out.println("\n...testing element2 TextSequenceList element.toString():\n");
+		System.out.println(testElement2.toString());
+		
+		
+		System.out.println("\n...testing TextSequenceList toString():\n");
+		System.out.println(testList.toString());
+		
+	}
+
+}
