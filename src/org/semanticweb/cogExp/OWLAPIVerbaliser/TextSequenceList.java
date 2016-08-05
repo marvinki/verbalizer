077ae003181a22ed20ff3f26a9372d0974f78112
f3l1xp <felix.paffrath@uni-ulm.de>
2016-08-05 17:17:25 +0200

    modified TextSequenceList (no single elements are returned as list), updated Doc

--- src/org/semanticweb/cogExp/OWLAPIVerbaliser/TextSequenceList.java
+++ src/org/semanticweb/cogExp/OWLAPIVerbaliser/TextSequenceList.java
@@ -4,117 +4,136 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * contains zero or more TextElementSequences. All you can do is create a list, add and get/set 
- * to elements in this List. 
+ * contains zero or more TextElementSequences. All you can do is create a list,
+ * add and get/set to elements in this List.
+ * 
  * @author fpaffrath
  *
  */
-public class TextSequenceList extends TextElement{
-	
+public class TextSequenceList extends TextElement {
+
 	private List<TextElementSequence> list = new ArrayList<TextElementSequence>();
 
-	
 	/**
-	 * @param list List of TextElements
+	 * @param list
+	 *            List of TextElements
 	 */
 	public TextSequenceList(List<TextElementSequence> list) {
 		this.list = list;
 	}
 
-	
+	public TextSequenceList() {
+	}
 
-	
-	public TextSequenceList(){}
-	
-	
 	/**
-	 * @param index the index of the desired element
-	 * @return the element at index
+	 * @param index
+	 *            the index of the desired element
+	 * @return returns the element at a certain index
 	 */
-	public TextElement getElement(int index){
-		
+	public TextElement getElement(int index) {
+
 		TextElement element = new TextElement();
-		
-		try{
+
+		try {
 			element = list.get(index);
-		}catch (Exception e) {
+		} catch (Exception e) {
 			// TODO: handle exception
 		}
-		
+
 		return element;
 	}
-	
-	
+
 	/**
-	 * @return number of elements in the list
+	 * @return returns the number of elements in the list
 	 */
-	public int getSize(){
+	public int getSize() {
 		return list.size();
 	}
-	
-	
+
 	/**
-	 * @param index where the element needs to be set
-	 * @param element  to set
+	 * @param index
+	 *            where the element needs to be set
+	 * @param element
+	 *            to set
 	 */
-	public void setElement( int index, TextElementSequence element){
-		
+	public void setElement(int index, TextElementSequence element) {
+
 		try {
 			list.set(index, element);
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
 	}
-	
 
 	/**
-	 * @param element to add at the end
+	 * @param sequence
+	 *            to add at the end
 	 */
-	public void add(TextElementSequence sequence){
+	public void add(TextElementSequence sequence) {
 		this.list.add(sequence);
 	}
-	
+
 	/**
 	 * 
 	 * @param element
+	 *            to add at the end
 	 */
-	public void add(TextElement element){
+	public void add(TextElement element) {
 		list.add(new TextElementSequence(element));
 	}
-	
-	
+
 	/**
-	 * @return all elements of the list as Strings, separated by line breaks
+	 * @return returns all elements of the list, that is longer than one element
+	 *         as Strings, separated by line breaks: "- element1 \n - element2
+	 *         \n - ... - elementN \n " If the list has exactly one element,
+	 *         then this function returns only this element ("element1"). If the
+	 *         list is empty it returns an empty string ("").
 	 */
 	@Override
-	public String toString(){
+	public String toString() {
 		String listSequence = "";
-		for(int i=0; i<list.size(); i++){
-				 listSequence = listSequence + "\n" + " - " + list.get(i).toString() ;
+
+		if (list.size() > 1) {
+			for (int i = 0; i < list.size(); i++) {
+				listSequence = listSequence + "\n" + " - " + list.get(i).toString();
+			}
 		}
+
+		if (list.size() == 1) {
+			listSequence = list.get(0).toString();
+		}
+
 		return listSequence;
 	}
-	
+
 	/**
-	 * @return todo
+	 * @return returns all elements of the list as HTML list:
+	 *         <ul>
+	 *         <li>element1</li>
+	 *         <li>element2</li> 
+	 *         <li> ... </li> 
+	 *         <li>elementN</li>
+	 *         </ul>
+	 * 
+	 *         If the list has exactly one element, then this function returns
+	 *         only this element. If the list is empty it returns an empty
+	 *         string ("").
 	 */
 	@Override
-	public String toHTML(){
-		String listSequence = "<ul>";
-		for(int i=0; i<list.size(); i++){
-				 listSequence = listSequence + "<li>" + list.get(i).toHTML() + "</li>" ;
+	public String toHTML() {
+		String listSequence = "";
+		if (list.size() > 1) {
+			listSequence += "<ul>";
+			for (int i = 0; i < list.size(); i++) {
+				listSequence += "<li>" + list.get(i).toHTML() + "</li>";
+			}
+			listSequence += "</ul>";
 		}
-		return listSequence + "</ul>";
+
+		if (list.size() == 1) {
+			listSequence = list.get(0).toHTML();
+		}
+
+		return listSequence;
 	}
-	
-	
-	/**
-	 * 
-	 * @return all elements of the list concatinated as one single TextElement
-	 */
-	
-//	public TextElement toTextElement(){
-//		TextElement te = new TextElement(this.toString());
-//		return te;
-//	}
 }
